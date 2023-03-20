437598bcfaa0f3d6af007d144ddb834e29809a0d
pedrothecatholic <pedro.sousa21@outlook.com>
2023-03-20 09:32:04 -0300

    Primeiro commit do curso de React Recoil

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,7 @@
+import style from "./Card.module.scss";
+
+const Card: React.FC = ({ children }) => {
+  return <div className={style.Card}>{children}</div>;
+};
+
+export default Card;
---
+++ src/components/Evento/EventoCheckbox/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IEvento } from "../../../interfaces/IEvento";
+import { listaDeEventosState } from "../../../state/atom";
+
+const EventoCheckbox: React.FC<{ evento: IEvento }> = ({ evento }) => {
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+
+  const alterarStatus = () => {
+    const eventoAlterado = {
+      ...evento
+    };
+    eventoAlterado.completo = !eventoAlterado.completo;
+
+    setListaDeEventos((listaAntiga) => {
+      const indice = listaAntiga.findIndex((e) => e.id === evento.id);
+
+      return [
+        ...listaAntiga.slice(0, indice),
+        evento,
+        ...listaAntiga.slice(indice + 1)
+      ];
+    });
+  };
+
+  const estilos = [
+    "far",
+    "fa-2x",
+    evento.completo ? "fa-check-square" : "fa-square"
+  ];
+
+  return <i className={estilos.join(" ")} onClick={alterarStatus}></i>;
+};
+
+export default EventoCheckbox;
